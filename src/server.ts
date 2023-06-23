25667e75ff584dca645cd7457ec892df0aa987e5
Omarabdullah99 <omarabdullah917303@gmail.com>
2023-06-23 16:47:06 +0600

    loggervdo12-3t012-7

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import { errorLogger, infoLogger } from './shared/logger'
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string) //as stirng na dile error ache
-    console.log(`Database connection successfully`)
+    infoLogger.info(`Database connection successfully`)
     app.listen(config.port, () => {
-      console.log(`Application app listening on port ${config.port}`)
+      infoLogger.info(`Application app listening on port ${config.port}`)
     })
   } catch (error) {
-    console.log('fail to connect database', error)
+    errorLogger.error('fail to connect database', error)
   }
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
